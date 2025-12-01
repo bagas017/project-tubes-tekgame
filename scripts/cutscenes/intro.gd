5c33da339c85dd2fea7c574ddf252bcc83fd7210
Dzaki Gastia - 122140030 <dzaki.122140030@student.itera.ac.id>
2025-12-01 17:58:30 +0700

    update intro

--- scripts/cutscenes/intro.gd
+++ scripts/cutscenes/intro.gd
@@ -6,5 +6,10 @@ func _ready():
 	# When video finishes → go to Main
 	video.finished.connect(_on_video_finished)
 
+func _process(delta):
+	# Tekan SPACE untuk skip
+	if Input.is_action_just_pressed("ui_accept"):  # SPACE / ENTER (default Godot)
+		_on_video_finished()
+		
 func _on_video_finished():
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
